00d5356afc541ab53df16b307c0f228df611787d
tsusdere <tsusdere@github.com>
2026-03-11 14:16:33 -0500

    add whitespace + numbers + chars

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -6,19 +6,59 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+	let ten = 10;
+	
+	let add = fn(x, y) {
+		x + y;
+		};
+		
+	let result = add(five, ten);`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 	}
 
@@ -56,6 +96,46 @@ func TestNextTokenUnicode(t *testing.T) {
 		{token.ILLEGAL, "é"},
 		{token.SEMICOLON, ";"},
 	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestGibberishInput(t *testing.T) {
+	input := `!-/*5;
+	5 < 10 > 5;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ILLEGAL, "!"},
+		{token.ILLEGAL, "-"},
+		{token.ILLEGAL, "/"},
+		{token.ILLEGAL, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		
+		{token.INT, "5"},
+		{token.ILLEGAL, "<"},
+		{token.INT, "10"},
+		{token.ILLEGAL, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+	}	
 
 	l := New(input)
 
